ceee91ce2682327ff5d2e1e4feacc8d58a00dafa
Ok_sana <sansen3000sansen@gmail.com>
2026-05-22 22:24:01 +0300

    init express project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Server is running',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
